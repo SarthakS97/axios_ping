f70418a6fa077ce9f22bf0cd913226b207e63baa
SarthakS97 <sarthak1509@gmail.com>
2023-03-21 19:23:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+
+// Replace "https://example.com" with the URL of the webpage you want to visit
+const url = "https://solforms.onrender.com";
+
+// Visit the webpage every 5 minutes
+setInterval(function() {
+  axios.get(url)
+    .then(response => console.log(response.data))
+    .catch(error => console.error(error));
+}, 9000); // 5 minutes in milliseconds
